3e0696f198c06d3b26e820bd6dc25fc4e070daf3
松野浩士 <amanojac52@gmail.com>
2020-04-08 18:06:56 +0900

    addList等定義

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <template v-for="list in lists">
+      <div class="list-container" :key="list.id">
+        <List :list="list" />
+      </div>
+    </template>
+    <input type="text" @change="addList" />
     <List v-for="list in lists" :key="list.id" :list="list" />
   </div>
 </template>
@@ -17,8 +23,23 @@ import { createInitialLists } from "./initialData";
 })
 export default class App extends Vue {
   lists: IList[] = createInitialLists();
+  listCreatedCount = 2;
+
+  addList(event: Event & { currentTarget: HTMLInputElement }): void {
+    const newList = {
+      id: this.listCreatedCount + 1,
+      name: event.currentTarget.value,
+      cards: [],
+    };
+    this.lists.push(newList);
+    //listsに追加されたため、listCreatedCountをインクリメント
+    ++this.listCreatedCount;
+    //フォームの値をリセットするために空文字を代入
+    event.currentTarget.value = "";
+  }
 }
 </script>
+
 <style lang="scss">
 #app {
   display: flex;
